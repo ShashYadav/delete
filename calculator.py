170d0121df1d737f959a9168272926b11cd58c5f
Shashank Dhirendra Kumar Yadav <shashank74265>
2025-03-28 12:38:46 +0530

    added addition code

---
+++ calculator.py
@@ -0,0 +1,5 @@
+a = int(input("Enter 1st number: "))  # Convert the input to integer
+b = int(input("Enter 2nd number: "))  # Convert the input to integer
+
+print("Sum is:", a + b)  # Fix the print statement to include a comma
+
